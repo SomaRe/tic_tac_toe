5005f9dbfb38a12d063378ded8fdb4345e592ee5
somaRe <shivatej82@gmail.com>
2019-11-11 00:47:24 +0530

    the end(mostly)

--- tictactoe.js
+++ tictactoe.js
@@ -1,57 +1,38 @@
 
 $(document).ready(function(){
     
-   $(".reset").click(function(){
-       location.reload(true);
-      /*var cells=document.querySelectorAll("td");
-       for(var i=0;i<9;i++){
-           cells[i].textContent='';
-       
-       $("#hl1").css("display","none");
-       $("#hl2").css("display","none");
-       $("#hl3").css("display","none");
-       $("#vl1").css("display","none");
-       $("#vl2").css("display","none");
-       $("#vl3").css("display","none");
-       $("#dl1").css("display","none");
-       $("#dl2").css("display","none");
-       }
-       $(".cell").each(function(){
-            $(this).prop("disabled",false);
-    });
-       $('.result').text("nil");
-       last="";*/
-       
-   });
-    
-    var last='';
+     var last='';
     
     function clickdisable(){
             $("td").off('click');
-            //$("#test").text($("td").prop("disabled", true));
-            
-                        }
+            }
+    
+    
+   $(".reset").click(function()
+       {
+       location.reload(true); 
+        });
+    
     
     $("td").click(function(){
-        
-         if(this.textContent==='' && last===''){
+         if(this.textContent==='' && last==='')
+         {
              this.textContent="X";
              last='X';
         }
-        
-        else if(last==='X'){
+        else if(last==='X')
+        {
             this.textContent="O";
             last='O';
         }
-        else if(last==='O'){ 
+        else if(last==='O')
+        { 
             this.textContent='X';
             last='X';
         }
-        else{
-            
-        }
-        
+       
         var winner = "nil"
+        
         //rows
     if($(".cell1").text() === $(".cell2").text() && $(".cell2").text()===$(".cell3").text() && $(".cell1").text()!=""){
         winner = $(".cell1").text();
@@ -68,6 +49,7 @@ $(document).ready(function(){
         $("#hl3").css("display","block");
         clickdisable();
     } 
+        
         //columns
     else if($(".cell1").text() === $(".cell4").text() && $(".cell4").text()===$(".cell7").text() && $(".cell1").text()!=""){
         winner = $(".cell1").text();
@@ -77,26 +59,27 @@ $(document).ready(function(){
     else if($(".cell2").text() === $(".cell5").text() && $(".cell5").text()===$(".cell8").text() && $(".cell2").text()!=""){
         winner = $(".cell2").text();
         $("#vl2").css("display","block");
-      clickdisable();
+        clickdisable();
     } 
     else if($(".cell3").text() === $(".cell6").text() && $(".cell6").text()===$(".cell9").text() && $(".cell3").text()!=""){
-            winner = $(".cell3").text();
+        winner = $(".cell3").text();
         $("#vl3").css("display","block");
-      clickdisable();
-            }
+        clickdisable();
+    }
+        
         //diagonal
      else if($(".cell1").text() === $(".cell5").text() && $(".cell5").text()===$(".cell9").text() && $(".cell1").text()!=""){
-            winner = $(".cell1").text();
-            $("#dl1").css("display","block");
-            clickdisable();
-            }
+        winner = $(".cell1").text();
+        $("#dl1").css("display","block");
+        clickdisable();
+    }
      else if($(".cell3").text() === $(".cell5").text() && $(".cell5").text()===$(".cell7").text() && $(".cell3").text()!="" ){
-            winner = $(".cell3").text();
-            $("#dl2").css("display","block");
-           clickdisable();
-            }    
+        winner = $(".cell3").text();
+        $("#dl2").css("display","block");
+        clickdisable();
+    }    
    
-         $(".result").text(winner); 
+      $(".result").text(winner);    
         
     
     });
